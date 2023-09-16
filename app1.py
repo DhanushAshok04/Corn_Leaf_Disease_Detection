f389140d0a85c9b6efe71779c9c2cecc98cb2d53
DhanushAshok04 <137204800+DhanushAshok04@users.noreply.github.com>
2023-09-17 04:09:58 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,24 +4,35 @@ from tensorflow.keras.models import load_model
 
 from PIL import Image
 import numpy as np
-st.title("Corn disease Image Classification")
 
+# Set a fixed background color using custom CSS
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #f0f0f0; /* Replace with your desired background color */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.title("Image Classification")
 st.write("Predict the Corn disease Image.")
 
 model = load_model("Model.h5")
-labels=['Blight', 'Common_Rust', 'Gray_Leaf_Spot', 'Healthy']
+labels = ['Blight', 'Common_Rust', 'Gray_Leaf_Spot', 'Healthy']
 uploaded_file = st.file_uploader(
     "Upload an image taken by Corn Disease:", type="jpg"
 )
-predictions=-1
+predictions = -1
 if uploaded_file is not None:
     image1 = Image.open(uploaded_file)
-    image1=image.smart_resize(image1,(224,224))
-    image1=classi=np.array(image1)/255.
-    result=model.predict(image1[np.newaxis,...])
-    
-    label=labels[np.argmax(result)]
-  
+    image1 = image.smart_resize(image1, (224, 224))
+    image1 = classi = np.array(image1) / 255.
+    result = model.predict(image1[np.newaxis, ...])
+
+    label = labels[np.argmax(result)]
 
 st.write("### Prediction Result")
 if st.button("Predict"):
@@ -33,20 +44,18 @@ if st.button("Predict"):
             unsafe_allow_html=True,
         )
     else:
-        st.write("Please upload file or choose sample image.")
-
+        st.write("Please upload a file or choose a sample image.")
 
 st.write("If you would not like to upload an image, you can use the sample image instead:")
 sample_img_choice = st.button("Use Sample Image")
 
 if sample_img_choice:
     image1 = Image.open("Corn_Health (7).jpg")
-    image1=image.smart_resize(image1,(224,224))
+    image1 = image.smart_resize(image1, (224, 224))
     img_array = image.img_to_array(image1)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array/255.0
+    img_array = img_array / 255.0
     predictions = model.predict(img_array)
-    label=labels[np.argmax(predictions)]
+    label = labels[np.argmax(predictions)]
     image1 = Image.open("Corn_Health (7).jpg")
-    st.image(image1, caption="Uploaded Image", use_column_width=True)    
-    st.mark
+    st.image(image1, caption="Uploaded Image", use_column_width=True)
